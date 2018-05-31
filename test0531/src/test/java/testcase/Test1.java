ff1420b37846e747a094b90f59f1d3a6884d3d2b
yiwenjing <574358627@qq.com>
2018-05-31 13:15:54 +0800

    added

---
+++ test0531/src/test/java/testcase/Test1.java
@@ -0,0 +1,14 @@
+package testcase;
+import org.testng.annotations.*;
+public class Test1 {
+	@BeforeClass
+	public void setup() {
+		
+	}
+
+	@Test
+	public void ywj() {
+		System.out.println("Hello word!");
+	}
+	
+}
